7d1ae9e0d624828f5ef19b20321faf7def61e46a
Abdullah Al Rakib <138372162+alrakib1@users.noreply.github.com>
2024-06-06 07:38:35 +0600

    Add social media icons and navigation links to the Navbar component

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,8 +1,25 @@
 import React from "react";
 import styles from "./navbar.module.css";
+import Image from "next/image";
+import Link from "next/link";
 
 const Navbar = () => {
-  return <div className={styles.container}>Navbar</div>;
+  return (
+    <div className={styles.container}>
+      <div className={styles.social}>
+        <Image src="/facebook.png" alt="facebook" width={24} height={24} />
+        <Image src="/instagram.png" alt="instagram" width={24} height={24} />
+        <Image src="/twitter.png" alt="twitter" width={24} height={24} />
+        <Image src="/linkedin.png" alt="linkedin" width={24} height={24} />
+      </div>
+      <div className={styles.logo}>RakibBlog</div>
+      <div className={styles.links}>
+        <Link href="/">Home</Link>
+        <Link href="/contact">Contact</Link>
+        <Link href="/about">About</Link>
+      </div>
+    </div>
+  );
 };
 
 export default Navbar;
---
+++ src/components/themeToggle/ThemeToggle.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import styles from './themeToggle.module.css';
+const ThemeToggle = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default ThemeToggle;
