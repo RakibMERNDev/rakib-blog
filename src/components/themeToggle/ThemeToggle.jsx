9a692c2241f784c0dec56f5b3896c1fcd643e5c4
Abdullah Al Rakib <138372162+alrakib1@users.noreply.github.com>
2024-08-29 11:51:25 +0600

    Update Navbar styles and links

--- src/components/themeToggle/ThemeToggle.jsx
+++ src/components/themeToggle/ThemeToggle.jsx
@@ -25,7 +25,7 @@ const ThemeToggle = () => {
             : { right: 1, background: "white" }
         }
       ></div>
-      <Image src="/moon.png" alt="" width={14} height={14} />
+      <Image src="/sun.png" alt="" width={14} height={14} />
     </div>
   );
 };
