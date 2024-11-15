23d56679654909bdea894f9413b3c5a9e4764fb9
abdullahalrakib727 <abdullahalrakib30@gmail.com>
2024-11-15 22:40:43 +0600

    Add Card component and enhance layout with responsive styles

---
+++ src/components/card/Card.jsx
@@ -0,0 +1,32 @@
+import Image from "next/image";
+import styles from "./card.module.css";
+import Link from "next/link";
+
+const Card = () => {
+  return (
+    <div className={styles.container}>
+      <div className={styles.imageContainer}>
+        <Image src="/p1.jpeg" alt="" fill className={styles.image}/>
+      </div>
+      <div className={styles.textContainer}>
+        <div className={styles.detail}>
+          <span className={styles.date}>22.11.2024 -</span>
+          <span className={styles.category}>CULTURE</span>
+        </div>
+        <h1>
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nulla,
+          veritatis?
+        </h1>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint delectus
+          ex inventore recusandae ipsam cupiditate placeat saepe quae. Nostrum
+          delectus deleniti aut facere ipsum labore magnam possimus voluptatum
+          animi aspernatur.
+        </p>
+        <Link href="/post/1">Read More</Link>
+      </div>
+    </div>
+  );
+};
+
+export default Card;
