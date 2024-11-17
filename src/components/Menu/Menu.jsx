e570e482f4f160c63ed0bccb4314dae48cf88305
abdullahalrakib727 <abdullahalrakib30@gmail.com>
2024-11-17 20:51:18 +0600

    Enhance Menu component with new layout, styles, and image handling

--- src/components/Menu/Menu.jsx
+++ src/components/Menu/Menu.jsx
@@ -1,9 +1,27 @@
 import React from "react";
 import styles from "./menu.module.css";
+import Link from "next/link";
+import Image from "next/image";
 
 const Menu = () => {
   return <div className={styles.container}>
-    <h2>{"What's hot"}</h2>
+    <h2 className={styles.subtitle}>{"What's hot"}</h2>
+    <h1 className={styles.title}>Most Popular</h1>
+    <div className={styles.items}>
+      <Link href='' className={styles.item}>
+      <div className={styles.imageContainer}>
+        <Image src="/p1.jpeg" alt="" className={styles.image} fill/>
+      </div>
+      <div className={styles.textContainer}>
+        <span className={`${styles.category} ${styles.travel}`}>Travel</span>
+        <h3 className={styles.postTitle}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis, ex!</h3>
+        <div className={styles.detail}>
+          <span className={styles.username}>Jhon Doe</span>
+          <span className={styles.date}>-10.11.24</span>
+        </div>
+      </div>
+      </Link>
+    </div>
   </div>;
 };
 
