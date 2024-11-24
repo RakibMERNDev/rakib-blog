21476363090ed261f561933fcd3fa38119ceeb77
abdullahalrakib727 <abdullahalrakib30@gmail.com>
2024-11-24 07:59:52 +0600

    Add SinglePage component with layout, styling, and comments section

--- src/app/[slug]/page.jsx
+++ src/app/[slug]/page.jsx
@@ -1,11 +1,61 @@
-import React from 'react';
+import Menu from "@/components/Menu/Menu";
+import styles from "./singlePage.module.css";
+import Image from "next/image";
 
 const SinglePage = () => {
-    return (
-        <div>
-            
+  return (
+    <div className={styles.container}>
+      <div className={styles.infoContainer}>
+        <div className={styles.textContainer}>
+          <h1 className={styles.title}>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos,
+            similique.
+          </h1>
+          <div className={styles.user}>
+            <div className={styles.userImageContainer}>
+              <Image src="/p1.jpeg" alt="" fill className={styles.avatar} />
+            </div>
+            <div className={styles.userTextContainer}>
+              <span className={styles.username}>Jhon Doe</span>
+              <span className={styles.date}>24.11.2024</span>
+            </div>
+          </div>
         </div>
-    );
+        <div className={styles.imageContainer}>
+          <Image src="/p1.jpeg" alt="" fill className={styles.image} />
+        </div>
+      </div>
+      <div className={styles.content}>
+        <div className={styles.post}>
+          <div className={styles.description}>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore
+              quae nisi adipisci, quaerat magni eius deleniti officiis et
+              repellendus quas laboriosam tempore incidunt sequi nemo commodi
+              dolorem non molestiae inventore.
+            </p>
+            <h2>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae,
+              ducimus!
+            </h2>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore
+              quae nisi adipisci, quaerat magni eius deleniti officiis et
+              repellendus quas laboriosam tempore incidunt sequi nemo commodi
+              dolorem non molestiae inventore.
+            </p>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore
+              quae nisi adipisci, quaerat magni eius deleniti officiis et
+              repellendus quas laboriosam tempore incidunt sequi nemo commodi
+              dolorem non molestiae inventore.
+            </p>
+          </div>
+        </div>
+        <Menu />
+      </div>
+    </div>
+  );
 };
 
 export default SinglePage;
