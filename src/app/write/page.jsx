d085ebe372774719f8a26185e8b524f90c56f3d2
abdullahalrakib727 <abdullahalrakib30@gmail.com>
2024-12-02 12:18:28 +0600

    add session handling and loading state to WritePage component

--- src/app/write/page.jsx
+++ src/app/write/page.jsx
@@ -1,20 +1,32 @@
 "use client";
 import Image from "next/image";
 import styles from "./write.module.css";
-import 'react-quill/dist/quill.bubble.css';
+import "react-quill/dist/quill.bubble.css";
 import ReactQuill from "react-quill";
 import { useState } from "react";
 
+import { useSession } from "next-auth/react";
+import { useRouter } from "next/navigation";
 
 const WritePage = () => {
   const [open, setOpen] = useState(false);
-  const [value, setValue] = useState('');
+  const [value, setValue] = useState("");
+
+  const router = useRouter();
+  const { status } = useSession();
+
+  if (status === "loading") {
+    return <div className={styles.loading}>Loading...</div>;
+  }
+  if (status === "authenticated") {
+    router.push("/");
+  }
 
   return (
     <div className={styles.container}>
-      <input type="text" placeholder="Title" className={styles.input}/>
+      <input type="text" placeholder="Title" className={styles.input} />
       <div className={styles.editor}>
-        <button className={styles.button} onClick={()=>setOpen(!open)}>
+        <button className={styles.button} onClick={() => setOpen(!open)}>
           <Image src="/plus.png" alt="" width={16} height={16} />
         </button>
         {open && (
@@ -30,7 +42,13 @@ const WritePage = () => {
             </button>
           </div>
         )}
-        <ReactQuill theme="bubble" value={value} onChange={setValue} placeholder="Tell your story..." className={styles.textArea}/>
+        <ReactQuill
+          theme="bubble"
+          value={value}
+          onChange={setValue}
+          placeholder="Tell your story..."
+          className={styles.textArea}
+        />
       </div>
       <button className={styles.publish}>Publish</button>
     </div>
