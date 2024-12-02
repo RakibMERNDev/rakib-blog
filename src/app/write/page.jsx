d63b0c3912ecddac899c80f1b4d66688e7977669
abdullahalrakib727 <abdullahalrakib30@gmail.com>
2024-12-02 10:11:40 +0600

    added react-quill editor

--- src/app/write/page.jsx
+++ src/app/write/page.jsx
@@ -1,13 +1,20 @@
+"use client";
 import Image from "next/image";
 import styles from "./write.module.css";
+import 'react-quill/dist/quill.bubble.css';
+import ReactQuill from "react-quill";
+import { useState } from "react";
+
+
 const WritePage = () => {
   const [open, setOpen] = useState(false);
+  const [value, setValue] = useState('');
 
   return (
     <div className={styles.container}>
       <input type="text" placeholder="Title" />
       <div className={styles.editor}>
-        <button className={styles.button}>
+        <button className={styles.button} onClick={()=>setOpen(!open)}>
           <Image src="/plus.png" alt="" width={16} height={16} />
         </button>
         {open && (
@@ -23,6 +30,7 @@ const WritePage = () => {
             </div>
           </>
         )}
+        <ReactQuill theme="bubble" value={value} onChange={setValue} placeholder="Tell your story..." />
       </div>
     </div>
   );
