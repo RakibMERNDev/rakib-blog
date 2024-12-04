3e5cc1235651e3498e42dd681d02c346367358a9
abdullahalrakib727 <abdullahalrakib30@gmail.com>
2024-12-04 13:41:50 +0600

    enhance pagination functionality in CardList component; update API to return post count and improve navigation buttons

--- src/components/pagination/Pagination.jsx
+++ src/components/pagination/Pagination.jsx
@@ -2,18 +2,20 @@
 import { useRouter } from "next/navigation";
 import styles from "./pagination.module.css";
 
-const Pagination = ({ page }) => {
+const Pagination = ({ page, hasPrev, hasNext }) => {
   const router = useRouter();
   return (
     <div className={styles.container}>
       <button
         className={styles.button}
+        disabled={!hasPrev}
         onClick={() => router.push(`?page=${page - 1}`)}
       >
         Previous
       </button>
       <button
         className={styles.button}
+        disabled={!hasNext}
         onClick={() => router.push(`?page=${page + 1}`)}
       >
         Next
